a5c90fdb6d39f06526796dfec9d2c91da2856a62
Wilan De Castro <65393800+wilandecastro@users.noreply.github.com>
2024-10-09 07:05:39 +0800

    Update main.tf
    edit template new code

--- main.tf
+++ main.tf
@@ -1,386 +1,81 @@
-#####################################
-##        NETWORK                  ##
-#####################################
-
-
-# Create the VPC
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
+
   tags = {
-    Name = var.vpc_name
+    Name        = var.vpc_name
     Environment = var.app_environment
-    map-migrated = var.ec2_tag
   }
 }
 
-# Define the public subnet-1
-resource "aws_subnet" "public-subnet-1" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.public_subnet_cidr-1
-  availability_zone = var.aws_az[0]
-  tags = {
-    Name = "${lower(var.vpc_name)}-${lower(var.app_environment)}-public-subnet-1"
-    Environment = var.app_environmentå
-    map-migrated = var.ec2_tag
-  }
-}
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr_1
+  availability_zone = "${var.aws_region}a"
 
-# Define the public subnet-2
-resource "aws_subnet" "public-subnet-2" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.public_subnet_cidr-2
-  availability_zone = var.aws_az[1]
   tags = {
-    Name = "${lower(var.vpc_name)}-${lower(var.app_environment)}-public-subnet-2"
+    Name        = "${var.vpc_name}-public-subnet-1"
     Environment = var.app_environment
-    map-migrated = var.ec2_tag
   }
 }
 
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr_2
+  availability_zone = "${var.aws_region}b"
 
-# Define the internet gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.vpc.id
   tags = {
-    Name = "${lower(var.vpc_name)}-${lower(var.app_environment)}-igw"
+    Name        = "${var.vpc_name}-public-subnet-2"
     Environment = var.app_environment
-    map-migrated = var.ec2_tag
   }
 }
 
-# Define the public route table
-resource "aws_route_table" "public-rt" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.vpc_name}-igw"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.main.id
   }
+
   tags = {
-    Name = "${lower(var.vpc_name)}-${lower(var.app_environment)}-public-subnet-rt"
+    Name        = "${var.vpc_name}-public-rt"
     Environment = var.app_environment
-    map-migrated = var.ec2_tag
   }
 }
 
-#Assign the public route table to the public subnet
-resource "aws_route_table_association" "public-rt-association-1" {
-  subnet_id      = aws_subnet.public-subnet-1.id
-  route_table_id = aws_route_table.public-rt.id
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "public-rt-association-2" {
-  subnet_id      = aws_subnet.public-subnet-2.id
-  route_table_id = aws_route_table.public-rt.id
- 
-}
-#Assign Static IP to instances
-
-# resource "aws_network_interface" "app" {
-#   subnet_id = aws_subnet.public-subnet-1.id
-#   private_ips = ["10.220.100.46"]
-#   tags =  {
-#     Name = "${lower(var.vpc_name)}-${lower(var.app_environment)}-app-interface"
-#   }
-# }
-
-# resource "aws_network_interface" "db" {
-#   subnet_id = aws_subnet.public-subnet-1.id
-#   private_ips = ["10.220.100.45"]
-#   tags = {
-#     Name = "${lower(var.vpc_name)}-${lower(var.app_environment)}-db-interface"
-#   }
-# }
-
-
-# resource "aws_network_interface" "lic" {
-#   subnet_id = aws_subnet.public-subnet-1.id
-#   private_ips = ["10.220.100.42"]
-#   tags = {
-#     Name = "${lower(var.vpc_name)}-${lower(var.app_environment)}-lic-interface"
-#   }
-# }
-
-
-###################################
-##              EC2              ##
-###################################
-
-#Bootstrapping PowerShell Script
-data "template_file" "windows-userdata" {
-  template = <<EOF
-<powershell>
-## Rename Machine
-## Rename-Computer -NewName "$#{var.windows_instance_name}" -Force;
-Set-LocalUser -Name "Administrator" -PasswordNeverExpires $true;
-Initialize-Disk -Number 1;
-New-Partition -DiskNumber 1 -UseMaximumSize -AssignDriveLetter | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Data";
-
-#### Modules listed in the Install Guide
-#Install-WindowsFeature -Name Web-Server -IncludeManagementTools;
-Install-WindowsFeature -Name Web-WebServer;
-Install-WindowsFeature -Name Web-Common-Http;
-Install-WindowsFeature -Name Web-Default-Doc;
-Install-WindowsFeature -Name Web-Dir-Browsing;
-Install-WindowsFeature -Name Web-Http-Errors;
-Install-WindowsFeature -Name Web-Static-Content;
-Install-WindowsFeature -Name Web-Http-Redirect;
-Install-WindowsFeature -Name Web-Health;
-Install-WindowsFeature -Name Web-Http-Logging;
-Install-WindowsFeature -Name Web-Custom-Logging;
-Install-WindowsFeature -Name Web-Log-Libraries;
-Install-WindowsFeature -Name Web-ODBC-Logging;
-Install-WindowsFeature -Name Web-Request-Monitor;
-Install-WindowsFeature -Name Web-Http-Tracing;
-Install-WindowsFeature -Name Web-Performance;
-Install-WindowsFeature -Name Web-Stat-Compression;
-Install-WindowsFeature -Name Web-Dyn-Compression;
-Install-WindowsFeature -Name Web-Security;
-Install-WindowsFeature -Name Web-Filtering;
-Install-WindowsFeature -Name Web-Digest-Auth;
-Install-WindowsFeature -Name Web-Url-Auth;
-Install-WindowsFeature -Name Web-Windows-Auth;
-Install-WindowsFeature -Name Web-AppInit;
-Install-WindowsFeature -Name Web-Net-Ext45;
-Install-WindowsFeature -Name Web-Asp-Net45;
-Install-WindowsFeature -Name Web-ISAPI-Ext;
-Install-WindowsFeature -Name Web-ISAPI-Filter;
-Install-WindowsFeature -Name Web-Includes;
-Install-WindowsFeature -Name Web-Mgmt-Tools;
-Install-WindowsFeature -Name Web-Mgmt-Console;
-Install-WindowsFeature -Name Web-Mgmt-Compat;
-Install-WindowsFeature -Name Web-Metabase;
-Install-WindowsFeature -Name Web-Lgcy-Mgmt-Console;
-Install-WindowsFeature -Name Web-Scripting-Tools;
-Install-WindowsFeature -Name Net-Framework-45-ASPNET;
-Install-WindowsFeature -Name NET-WCF-HTTP-Activation45;
-Install-WindowsFeature -Name WAS;
-Install-WindowsFeature -Name WAS-Process-Model;
-Install-WindowsFeature -Name WAS-Config-APIs;
-#### Additional Common Modules
-Install-WindowsFeature -Name Web-Basic-Auth;
-Install-WindowsFeature -Name Web-Windows-Auth;
-Install-WindowsFeature -Name Telnet-Client;
-
-
-#### Firewall rules
-Enable-NetFirewallRule -DisplayName "World Wide Web Services (HTTP Traffic-In);
-Enable-NetFirewallRule -DisplayName "World Wide Web Services (HTTPS Traffic-In);
-New-NetFirewallRule -DisplayName "Camstar Auth to Active Directory" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 88;
-New-NetFirewallRule -DisplayName "Camstar LDAP Auth" -Direction Inbound -Action Allow -Protocol tcp -LocalPort 389;
-New-NetFirewallRule -DisplayName "Camstar LDAP Auth-SSL" -Direction Inbound -Action Allow -Protocol tcp -LocalPort 636;
-New-NetFirewallRule -DisplayName "Camstar Management Studio" -Direction Inbound -Action Allow -Protocol tcp -LocalPort 2882;
-New-NetFirewallRule -DisplayName "Microsoft Exchange Notification" -Direction Inbound -Action Allow -Protocol tcp -LocalPort 2883;
-New-NetFirewallRule -DisplayName "MS SQL Server 2019" -Direction Inbound -Action Allow -Protocol tcp -LocalPort 1433;
-####### For License Server
-New-NetFirewallRule -DisplayName "Siemens PLM License Server - Flexera - lmgrd.exe" -Direction Inbound -Action Allow -Protocol tcp -LocalPort 28000;
-New-NetFirewallRule -DisplayName "Siemens PLM License Server - Licensing Component - ugslmd.exe" -Direction Inbound -Action Allow -Protocol tcp -LocalPort 29000;
-
-# Restart machine
-shutdown -r -t 10;
-</powershell>
-EOF
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public.id
 }
 
-#####################
-## Key Pair - Main ##
-#####################
-
-# Generates a secure private key and encodes it as PEM
-resource "tls_private_key" "key_pair" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-# Create the Key Pair
-resource "aws_key_pair" "key_pair" {
-  key_name   = "${lower(var.vpc_name)}-${lower(var.app_environment)}-windows-${lower(var.aws_region)}"  
-  public_key = tls_private_key.key_pair.public_key_openssh
-}
-
-# Save file
-resource "local_file" "ssh_key" {
-  filename = "${aws_key_pair.key_pair.key_name}.pem"
-  content  = tls_private_key.key_pair.private_key_pem
-}
-
-# data "aws_iam_role" "example" {
-#   name = "AmazonSSMRoleForInstancesQuickSetup"
-# }
-
-# Create EC2 Instance
-resource "aws_instance" "windows-server-app1" {
-  ami                         = "ami-0d67743feaea944bf"
-  instance_type               = var.windows_instance_type[0]
-  subnet_id                   = aws_subnet.public-subnet-1.id
-  vpc_security_group_ids      = [aws_security_group.aws-windows-sg.id]
-  associate_public_ip_address = var.windows_associate_public_ip_address
-  source_dest_check           = false
-  key_name                    = aws_key_pair.key_pair.key_name
-  #iam_instance_profile        =  data.aws_iam_role.example.name
-  user_data                   = data.template_file.windows-userdata.rendered
-  
-  # root disk
-  root_block_device {
-    volume_size           = var.windows_root_volume_size_app
-    volume_type           = var.windows_root_volume_type
-    delete_on_termination = true
-    encrypted             = true
-  }
-
-  # extra disk
-  ebs_block_device {
-    device_name           = "/dev/sdf"
-    volume_size           = var.windows_data_volume_size_app
-    volume_type           = var.windows_data_volume_type
-    encrypted             = true
-    delete_on_termination = true
-  }
-
-  # # Static IP address
-  # network_interface {
-  #    network_interface_id = "${aws_network_interface.app.id}"
-  #    device_index = 0
-  # }
-  
-   tags = {
-    Name        = "${var.app_name_app}.${var.app_stg_app}"
-    Environment = var.app_environment
-    Customer     = var.customer_name
-    map-migrated = var.ec2_tag
-  }
-}
-
-
-
-
-
-# Create EC2 Instance - report database
-
-resource "aws_instance" "windows-server-db1" {
-  ami                         = "ami-0d67743feaea944bf"
-  instance_type               = var.windows_instance_type[0]
-  subnet_id                   = aws_subnet.public-subnet-1.id
-  vpc_security_group_ids      = [aws_security_group.aws-windows-sg.id]
-  associate_public_ip_address = var.windows_associate_public_ip_address
-  source_dest_check           = false
-  key_name                    = aws_key_pair.key_pair.key_name
-  #iam_instance_profile        =  data.aws_iam_role.example.name
-  user_data                   = data.template_file.windows-userdata.rendered
-  
-  # root disk
-  root_block_device {
-    volume_size           = var.windows_root_volume_size_db
-    volume_type           = var.windows_root_volume_type
-    delete_on_termination = true
-    encrypted             = true
-  }
-
-  # extra disk
-  ebs_block_device {
-    device_name           = "/dev/sdf"
-    volume_size           = var.windows_data_volume_size_db
-    volume_type           = var.windows_data_volume_type
-    encrypted             = true
-    delete_on_termination = true
-  }
-  # network_interface {
-  #    network_interface_id = "${aws_network_interface.db.id}"
-  #    device_index = 0
-  # }
-  
-  tags = {
-    Name        = "${var.app_name_db1}.${var.app_stg_app}"
-    Environment = var.app_environment
-    Customer     = var.customer_name
-    map-migrated = var.ec2_tag
-  }
-}
-
-
-
-# Create EC2 Instance - LIC Server
-
-resource "aws_instance" "windows-server-lic" {
-  ami                         = "ami-0d67743feaea944bf"
-  instance_type               = var.windows_instance_type[1]
-  subnet_id                   = aws_subnet.public-subnet-1.id
-  vpc_security_group_ids      = [aws_security_group.aws-windows-sg.id]
-  associate_public_ip_address = var.windows_associate_public_ip_address
-  source_dest_check           = false
-  key_name                    = aws_key_pair.key_pair.key_name
-  #iam_instance_profile        =  data.aws_iam_role.example.name
-  user_data                   = data.template_file.windows-userdata.rendered
-  
-  # root disk
-  root_block_device {
-    volume_size           = var.windows_root_volume_size_lic
-    volume_type           = var.windows_root_volume_type
-    delete_on_termination = true
-    encrypted             = true
-  }
-
-  # extra disk
-  ebs_block_device {
-    device_name           = "/dev/sdf"
-    volume_size           = var.windows_data_volume_size_lic
-    volume_type           = var.windows_data_volume_type
-    encrypted             = true
-    delete_on_termination = true
-  }
-  # network_interface {
-  #    network_interface_id = "${aws_network_interface.lic.id}"
-  #    device_index = 0
-  # }
-  
-  tags = {
-    Name        = "${var.app_name_lic1}.${var.app_stg_app}"
-    Environment = var.app_environment
-    Customer     = var.customer_name
-    map-migrated = var.ec2_tag
-  }
-}
-
-
-# Define the security group for the Windows server
-
-resource "aws_security_group" "aws-windows-sg" {
-  name        = "${lower(var.vpc_name)}-${var.app_environment}-windows-sg"
-  description = "Allow incoming connections"
-  vpc_id      = aws_vpc.vpc.id
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
+    description = "HTTP from VPC"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.220.100.32/27"]
-    description = "Allow incoming HTTP connections"
+    cidr_blocks = [aws_vpc.main.cidr_block]
   }
 
- ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks =   [ "10.220.100.32/27" ]
-    description = "Allow incoming RDP connections"
-  }
- 
-  
-  ingress {
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
-    cidr_blocks =   [ "112.204.167.121/32" ]
-    description = "Allow incoming RDP connections"
-  }
- 
-  ingress {
-    from_port   = 1194
-    to_port     = 1194
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow incoming RDP connections"
-  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -389,39 +84,6 @@ resource "aws_security_group" "aws-windows-sg" {
   }
 
   tags = {
-    Name        = "${lower(var.vpc_name)}-${var.app_environment}-windows-sg"
-    Environment = var.app_environment
+    Name = "allow_http"
   }
 }
-
-
-
-
-resource "aws_eip" "windows-eip-app" {
-  count = 3
-  vpc = true
-
-  tags = {
-      Name        = "${lower(var.vpc_name)}-${var.app_environment}-windows-eip-app"
-      Environment = var.app_environment
-  }
-}
-
-resource "aws_eip_association" "app1" {
-  instance_id = aws_instance.windows-server-app1.id
-  allocation_id = aws_eip.windows-eip-app[0].id
-}
-
-resource "aws_eip_association" "db1" {
-  instance_id = aws_instance.windows-server-db1.id
-  allocation_id = aws_eip.windows-eip-app[1].id
-}
-
-resource "aws_eip_association" "lic" {
-  instance_id = aws_instance.windows-server-lic.id
-  allocation_id = aws_eip.windows-eip-app[2].id
-}
-
-
-
-
